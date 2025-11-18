5f9027af2b444976a816e93c37d7f4f705ccc6d1
abdullahalnomandev <abdullahalnoman1512@gmail.com>
2025-11-18 18:30:04 +0600

    update

--- src/pages/dashboard/transactions/index.tsx
+++ src/pages/dashboard/transactions/index.tsx
@@ -1,5 +1,3 @@
-'use client';
-
 import { useState } from 'react';
 import { Button, ConfigProvider, Input, Select, Table, Tabs } from 'antd';
 import type { ColumnType } from 'antd/es/table/interface';
